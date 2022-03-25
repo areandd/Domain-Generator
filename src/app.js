120200c68ead6e77086080d4c96cba91543bc3c5
Isaiah Desta <areandd@protonmail.com>
2022-03-25 16:07:04 +0000

    used previous code / repurposed for domain name generator utilizing small data set.

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#button").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = excuseGen();
+  });
+
+  // let randomNumber = Math.random() * 10;
+  // console.log("Hello Rigo from the console!" + randomNumber);
+};
+
+let excuseGen = () => {
+  let one = ["god", "git", "faze", "gantry"];
+  let two = ["crane", "send", "collapse", "r"];
+  let three = ["me", "the", "rotate", "society"];
+  let com = [".com", ".io", ".edu", ".co"];
+
+  let oneIndex = Math.floor(Math.random() * one.length);
+  let twoIndex = Math.floor(Math.random() * two.length);
+  let threeIndex = Math.floor(Math.random() * three.length);
+  let comIndex = Math.floor(Math.random() * com.length);
+
+  return `${one[oneIndex]}${two[twoIndex]}${three[threeIndex]}${com[comIndex]}`;
 };
