00b723484756d662f13c43444777a504757662a3
Isaiah Desta <areandd@protonmail.com>
2022-07-25 14:22:10 +0000

    restructured to display all at once

---
+++ src/index.js
@@ -0,0 +1,24 @@
+document.querySelector("#btn").addEventListener("click", () => {
+  document.querySelector("#name").innerHTML = domainName();
+});
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+function domainName() {
+  let i = 0;
+  let j = 0;
+  let k = 0;
+
+  let temp = "";
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        temp += pronoun[i] + adj[j] + noun[k] + ".com <br>";
+      }
+    }
+  }
+  return temp;
+}
